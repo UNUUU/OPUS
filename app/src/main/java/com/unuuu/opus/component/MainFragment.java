e354840ab968687066296ad5addcd26bb57940c9
t-kashima <takumi.kashima@nanapi.co.jp>
2016-02-21 16:12:59 +0900

    プロジェクトをリファクタリングしました

--- app/src/main/java/com/unuuu/opus/component/MainFragment.java
+++ app/src/main/java/com/unuuu/opus/component/MainFragment.java
@@ -19,84 +19,85 @@ import com.unuuu.opus.event.TakePictureEvent;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-/**
- * Created by kashima on 15/06/15.
- */
 public class MainFragment extends Fragment {
-    /** フラッシュが有効かどうか */
-    private boolean mIsFlashOn;
+    /**
+     * フラッシュが有効かどうか
+     */
+    private boolean isFlashOn = false;
 
-    /** カメラ周りの処理をする */
-    private CameraPreview mCameraPreview;
+    /**
+     * カメラ周りの処理をする
+     */
+    private CameraPreview cameraPreview;
 
-    @Bind(R.id.fragment_main_frame_001)
-    ImageView mFlashButton;
+    @Bind(R.id.fragment_main_image_flash)
+    ImageView flashButton;
 
-    @Bind(R.id.fragment_main_layout_002)
-    FrameLayout mCameraLayout;
+    @Bind(R.id.fragment_main_layout_camera)
+    FrameLayout cameraLayout;
 
-    // View作成
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         ButterKnife.bind(this, rootView);
 
-        mIsFlashOn = false;
+        initCamera();
 
-        // Flashの切り替えをする
-        mFlashButton.setOnClickListener(new View.OnClickListener() {
+        flashButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mIsFlashOn = !mIsFlashOn;
-
-                if (mIsFlashOn) {
-                    mFlashButton.setImageResource(R.drawable.main_flash_on);
-                } else {
-                    mFlashButton.setImageResource(R.drawable.main_flash_off);
-                }
-
-                BusHolder.get().post(new ChangeFlashModeEvent(mIsFlashOn));
+                toggleFlash();
             }
         });
 
-        mCameraPreview = new CameraPreview(getActivity().getApplicationContext());
-        mCameraLayout.addView(mCameraPreview);
-
         return rootView;
+    }
+
+    /**
+     * カメラを初期化する
+     */
+    private void initCamera() {
+        cameraPreview = new CameraPreview(getActivity().getApplicationContext());
+        cameraLayout.addView(cameraPreview);
+    }
+
+    /**
+     * フラッシュを設定する
+     */
+    private void toggleFlash() {
+        isFlashOn = !isFlashOn;
+
+        if (isFlashOn) {
+            flashButton.setImageResource(R.drawable.main_flash_on);
+        } else {
+            flashButton.setImageResource(R.drawable.main_flash_off);
+        }
+
+        BusHolder.get().post(new ChangeFlashModeEvent(isFlashOn));
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
         BusHolder.get().register(this);
     }
 
     @Override
     public void onPause() {
-        BusHolder.get().unregister(this);
-
         super.onPause();
+        BusHolder.get().unregister(this);
     }
 
     @Override
     public void onDestroyView() {
-        ButterKnife.unbind(this);
-
         super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 
-    @SuppressWarnings("unused")
-    @Subscribe
-    public void subscribe(TakePictureEvent event) {
-        // 写真撮影をする
-        this.mCameraPreview.takePicture();
-    }
 
-    @SuppressWarnings("unused")
     @Subscribe
-    public void subscribe(SavedImageEvent event) {
-        PreviewActivity.startActivity(getActivity(), event.mImagePath);
+    public void subscribe(@SuppressWarnings("unused") TakePictureEvent event) {
+        cameraPreview.takePicture();
     }
 }
